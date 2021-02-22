c104726de688a9e3a0b044b70683cfaf98f95e47
emretr10 <kahvecilertv@gmail.com>
2021-02-22 13:17:50 +0000

    Initial commit

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,251 @@
+import React, { useState, useEffect } from 'react'
+import { Box, Typography, makeStyles, Button, Link } from '@material-ui/core'
+import { Helmet } from 'react-helmet'
+import * as Service from '../../services'
+import liquidityIcon from '../../assets/images/liquidity.png'
+import volumeIcon from '../../assets/images/volume.png'
+import { reduceLongNumber, redirectToUrl } from '../../utils'
+import '../../'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    // minHeight: '100vh',
+    color: theme.color.text.main,
+    fontFamily: '"Nunito Sans", sans-serif !important',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      margin: '20px 20px 100px 20px',
+    },
+    [theme.breakpoints.up('md')]: {
+      margin: '100px 80px 0px 80px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: '155px 135px 0px 135px',
+    },
+  },
+  titleContainer: {
+    marginBottom: 65,
+  },
+  introText: {
+    fontSize: 40,
+    fontWeight: 800,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 32,
+    },
+  },
+  statContainer: {
+    flexWrap: 'wrap',
+    marginBottom: 100,
+    gap: 95,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: 65,
+    },
+    [theme.breakpoints.down('md')]: {
+      gap: 75,
+    },
+  },
+  statItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  statIcon: {
+    width: 40,
+    height: 40,
+    marginBottom: 20,
+  },
+  statLabel: {
+    marginBottom: 5,
+    color: theme.color.text.main,
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  statValue: {
+    color: theme.color.text.highlighted,
+    fontSize: 28,
+    fontWeight: 700,
+  },
+  launchBtn: {
+    padding: '14px 20px',
+    width: 230,
+    height: 48,
+    borderRadius: 8,
+    backgroundColor: theme.color.primary.main,
+    color: theme.color.text.dark,
+    fontSize: 18,
+    fontWeight: 800,
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: theme.color.primary.dark,
+    },
+    [theme.breakpoints.down('sm')]: {
+      margin: 20,
+      width: 'calc(100vw - 40px)',
+    },
+  },
+  homepageImg: {
+    width: '100%',
+    height: 'auto',
+    [theme.breakpoints.up('md')]: {
+      width: 400,
+      maxWidth: '50%',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: 500,
+    },
+  },
+  highlightedText: {
+    color: theme.color.text.primary1,
+    fontSize: 'inherit',
+    fontWeight: 800,
+  },
+  mobileLaunchContainer: {
+    display: 'inline',
+    [theme.breakpoints.down('sm')]: {
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: theme.color.background.light,
+    },
+  },
+  socialLink: {
+    color: theme.color.text.main,
+    fontSize: 14,
+    fontWeight: 600,
+    cursor: 'pointer',
+    '&:not(:first-child)': {
+      marginLeft: 50,
+    },
+    [theme.breakpoints.down('sm')]: {
+      '&:not(:first-child)': {
+        marginLeft: 0,
+      },
+    },
+  },
+}))
+
+const Homepage = () => {
+  const [totalLiquidity, setTotalLiquidity] = useState(0)
+  const [volume, setVolume] = useState(0)
+  const classes = useStyles()
+
+  useEffect(() => {
+    Service.getTotalLiquidityData().then((data) => {
+      setTotalLiquidity(data.totalLiquidity)
+    })
+    Service.get24hVolumeChange().then((data) => {
+      setVolume(data.value)
+    })
+  }, [])
+
+  return (
+    <Box display="flex" flexDirection="column" minHeight="calc(100vh - 70px)">
+      <Helmet>
+        <title>{'Welcome to LuaSwap'}</title>
+      </Helmet>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        className={classes.root}
+      >
+        <Box mr={[0, 4, 5]}>
+          <Box className={classes.titleContainer}>
+            <Typography component="div" className={classes.introText}>
+              {'Multi-chain liquidity protocol for '}
+              <Typography component="div" className={classes.highlightedText}>
+                {'emerging token projects'}
+              </Typography>
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            alignItems="flex-start"
+            className={classes.statContainer}
+          >
+            <Box className={classes.statItem}>
+              <img
+                alt="Total Liquidity"
+                src={liquidityIcon}
+                className={classes.statIcon}
+              />
+              <Typography component="div" className={classes.statLabel}>
+                {'Total Liquidity'}
+              </Typography>
+              <Typography component="div" className={classes.statValue}>
+                {`${reduceLongNumber(totalLiquidity)} USD`}
+              </Typography>
+            </Box>
+            <Box className={classes.statItem}>
+              <img
+                alt="24h Volume"
+                src={volumeIcon}
+                className={classes.statIcon}
+              />
+              <Typography component="div" className={classes.statLabel}>
+                {'24h Volume'}
+              </Typography>
+              <Typography component="div" className={classes.statValue}>
+                {`${reduceLongNumber(volume)} USD`}
+              </Typography>
+            </Box>
+          </Box>
+          <Box className={classes.mobileLaunchContainer}>
+            <Button
+              variant="contained"
+              className={classes.launchBtn}
+              onClick={() => redirectToUrl('https://app.luaswap.org/')}
+            >
+              {'Launch App'}
+            </Button>
+          </Box>
+        </Box>
+        <img
+          alt="Homepage"
+          src="https://raw.githubusercontent.com/tomochain/luaswap-homepage/gh-pages/static/media/homepage.1a9b09bf.png"
+          className={classes.homepageImg}
+        />
+      </Box>
+      <Box
+        display="flex"
+        justifyContent={['space-around', 'center']}
+        mt="auto"
+        mb={[13, 3]}
+      >
+        <Link
+          onClick={() =>
+            redirectToUrl('https://github.com/tomochain/luaswap-core')
+          }
+          className={classes.socialLink}
+        >
+          {'GitHub'}
+        </Link>
+        <Link
+          onClick={() => redirectToUrl('https://twitter.com/luaswap')}
+          className={classes.socialLink}
+        >
+          {'Twitter'}
+        </Link>
+        <Link
+          onClick={() => redirectToUrl('https://t.me/luaswap')}
+          className={classes.socialLink}
+        >
+          {'Telegram'}
+        </Link>
+        <Link
+          onClick={() => redirectToUrl('https://medium.com/luaswap')}
+          className={classes.socialLink}
+        >
+          {'Medium'}
+        </Link>
+      </Box>
+    </Box>
+  )
+}
+
+export default Homepage
---
+++ src/services/index.js
@@ -0,0 +1,187 @@
+import axios from 'axios'
+import BigNumber from 'bignumber.js'
+import _get from 'lodash.get'
+import { FACTORY_ADDRESS } from '../sushi/lib/constants'
+
+const LUA_TOKEN_ADDRESS = '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
+const route = axios.create({
+  baseURL: 'https://wallet.tomochain.com/api/luaswap',
+})
+const graphRoute = axios.create({
+  baseURL: 'https://api.thegraph.com/subgraphs/name/phucngh/luaswap',
+})
+const blockRoute = axios.create({
+  baseURL:
+    'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+})
+
+export const getLuaPrice = (callback) => {
+  return route
+    .get(`/price/LUA`)
+    .then((res) => {
+      if (typeof callback === 'function') {
+        return callback(res)
+      }
+      return _get(res, 'data.usdPrice')
+    })
+    .catch((err) => {
+      console.error('[ERROR]:', err)
+      return 0
+    })
+}
+
+export const getTotalSupply = (callback) => {
+  const method = 'totalSupply():(uint256)'
+  const cache = true
+
+  return route
+    .post(`/read/${LUA_TOKEN_ADDRESS}`, {
+      method,
+      cache,
+      params: [],
+    })
+    .then((res) => {
+      const totalSupply = _get(res, 'data.data')
+      if (totalSupply) {
+        const convertedNumber = new BigNumber(totalSupply)
+          .div(10 ** 18)
+          .toNumber()
+        if (typeof callback === 'function') {
+          return callback(convertedNumber)
+        }
+        return convertedNumber
+      }
+      return 0
+    })
+    .catch((err) => {
+      console.error('[ERROR]:', err)
+      return 0
+    })
+}
+
+export const getTotalLiquidityData = (callback) => {
+  const operationName = 'transactions'
+  const query = `query transactions {
+      uniswapFactories(
+        first: 100
+      ) {
+        id
+        totalLiquidityUSD
+        totalVolumeUSD
+      }
+    }`
+  const variables = {}
+
+  return graphRoute
+    .post('', {
+      operationName,
+      query,
+      variables,
+    })
+    .then((res) => {
+      if (_get(res, 'data.errors')) {
+        console.error('[ERROR]:', _get(res, 'data.errors.message', ''))
+        return {}
+      }
+      const result = _get(res, 'data.data.uniswapFactories', []).map(
+        (item) => ({
+          totalLiquidity: item.totalLiquidityUSD,
+          volume: item.totalVolumeUSD,
+        }),
+      )[0]
+
+      if (typeof callback === 'function') {
+        return callback(result)
+      }
+      return result
+    })
+    .catch((err) => {
+      console.error('[ERROR]:', err)
+      return {}
+    })
+}
+
+export const getBlockFromTimestamp = async (timestamp) => {
+  const result = await blockRoute.post('', {
+    query: `{
+      blocks(
+        first: 1
+        orderBy: timestamp
+        orderDirection: asc
+        where: {
+          ${timestamp ? `timestamp_gt: ${timestamp}` : ''}
+        }
+      ) {
+        id
+        number
+        timestamp
+      }
+    }`,
+  })
+
+  if (
+    !result ||
+    _get(result, 'data.errors') ||
+    _get(result, 'data.data.blocks.length') === 0
+  ) {
+    throw new Error('Failed to fetch block number from the subgraph.')
+  }
+
+  return _get(result, ['data', 'data', 'blocks', 0, 'number'])
+}
+
+const getFactoryQuery = (block) => {
+  return `query uniswapFactories {
+    uniswapFactories(
+      ${block ? `block: { number: ${block} }` : ''}
+      where: {
+        id: "${FACTORY_ADDRESS}"
+      }
+    ) {
+      id
+      totalVolumeUSD
+    }
+  }`
+}
+
+export const get24hVolumeChange = async () => {
+  const currentTime = Math.floor(new Date().getTime() / 1000)
+  const lastDayBlockNumber = await getBlockFromTimestamp(currentTime - 86400)
+  const [volumeData, lastDayVolumeData] = await Promise.all([
+    await graphRoute.post('', {
+      query: getFactoryQuery(),
+    }),
+    await graphRoute.post('', {
+      query: getFactoryQuery(lastDayBlockNumber),
+    }),
+  ])
+
+  if (
+    !volumeData ||
+    !lastDayVolumeData ||
+    _get(volumeData, 'data.errors') ||
+    _get(lastDayVolumeData, 'data.errors')
+  ) {
+    throw new Error('Failed to fetch volume changes from subgraph')
+  }
+
+  const currentVolume = Number(
+    _get(
+      volumeData,
+      ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
+      0,
+    ),
+  )
+  const lastDayVolume = Number(
+    _get(
+      lastDayVolumeData,
+      ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
+      0,
+    ),
+  )
+
+  return {
+    value: currentVolume - lastDayVolume,
+    percent: (currentVolume / lastDayVolume - 1) * 100,
+  }
+}
